d437c398d40ea471354e32c218dc1ab29ec908dc
neotechz <marc.ayalde@gmail.com>
2025-01-28 16:47:41 +0800

    Modify bin/make-manifest.py to take the working directory as the first argument

--- bin/make-manifest.py
+++ bin/make-manifest.py
@@ -4,13 +4,14 @@ import pandas as pd
 import sys
 
 def main():
-    fpath= f"{sys.argv[1]}"
+    cwd = f"{sys.argv[1]}/data/"
+    fpath = f"{cwd}{sys.argv[2]}"
     sample = fpath.split("/")[-1].rsplit("-", 2)[0]
 
     sampleIDs = [sample]
     abs_path = [fpath]
 
-    manifest =  pd.DataFrame({'sampleID': sampleIDs, 'absolute-filepath': abs_path}) 
+    manifest =  pd.DataFrame({'sampleID': sampleIDs, 'absolute-filepath': abs_path})
     with open(f"{sample.split('.')[0]}.tsv", 'w+') as m:
         print(manifest.to_csv(sep='\t', index=False, header=True), file=m)
 
