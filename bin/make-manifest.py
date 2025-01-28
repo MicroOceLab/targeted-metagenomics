d7cda906621b0bf1e4c07d39dfb319b4b509e5f5
neotechz <marc.ayalde@gmail.com>
2025-01-28 15:14:18 +0800

    Modify bin/make-manifest.py to use relative paths (including work) and into executable

--- bin/make-manifest.py
+++ bin/make-manifest.py
@@ -1,16 +1,17 @@
+#!/usr/bin/env python3
+
 import pandas as pd
-import os
 import sys
 
 def main():
-    fpath= f"{os.getcwd()}/data/{sys.argv[1]}"
+    fpath= f"{sys.argv[1]}"
     sample = fpath.split("/")[-1].rsplit("-", 2)[0]
 
     sampleIDs = [sample]
     abs_path = [fpath]
 
     manifest =  pd.DataFrame({'sampleID': sorted(sampleIDs), 'absolute-filepath': sorted(abs_path)}) 
-    with open(f"{os.getcwd()}/results/1-data-prep/{sample}.txt", 'w+') as m:
+    with open(f"{sample.split('.')[0]}.txt", 'w+') as m:
         print(manifest.to_csv(sep='\t', index=False, header=True), file=m)
 
 main()
