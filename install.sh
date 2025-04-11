34b4c67d237fe3a6c3810655e02f323055646c04
neotechz <marc.ayalde@gmail.com>
2025-04-11 14:04:40 +0800

    Add docker images for FASTQC and MULTIQC in install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 docker build --file assets/python.Dockerfile --tag MicroOceLab/python:1.0 .
+docker pull quay.io/biocontainers/fastqc:0.11.2--pl5.22.0_0
+docker pull quay.io/biocontainers/multiqc:1.28--pyhdfd78af_0
 docker pull quay.io/qiime2/amplicon:2024.10
 
 mkdir -p data
