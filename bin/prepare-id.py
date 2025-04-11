cfc89ad8df719d38c8dd72648377360a7621fda0
neotechz <marc.ayalde@gmail.com>
2025-04-11 17:50:00 +0800

    Fix missing split functions in prepare-id.py

--- bin/prepare-id.py
+++ bin/prepare-id.py
@@ -7,6 +7,13 @@ def main():
     fpath = f"{sys.argv[1]}"
     id = fpath.split("/")[-1].rsplit(".", 2)[0]
     
+    if "_" in id:
+        id = id.rsplit("_", 2)[0]
+    elif "-" in id:
+        id = id.rsplit("-", 2)[0]
+    else:
+        id = id
+
     print(id)
 
 main()
