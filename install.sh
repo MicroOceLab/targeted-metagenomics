0c511224f416b7f17bb53cdcf8d8ce765a12c894
neotechz <marc.ayalde@gmail.com>
2025-04-12 11:32:49 +0800

    Replace FASTQC Docker image with newer version (0.11.7--4)

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 docker build --file assets/python.Dockerfile --tag MicroOceLab/python:1.0 .
-docker pull quay.io/biocontainers/fastqc:0.11.2--pl5.22.0_0
+docker pull quay.io/biocontainers/fastqc:0.11.7--4
 docker pull quay.io/biocontainers/multiqc:1.28--pyhdfd78af_0
 docker pull quay.io/qiime2/amplicon:2024.10
 
